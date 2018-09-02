37b685735638ef40fac414960daac6a5b95529e3
chrikin1 <chrisianbartoo@gmail.com>
2018-09-03 00:21:37 +0300

    styling and conf of app

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ An application that list and preview news articles from various sources.
 - Users are parents or people who viw later the news missed
 - Users are able to see all the news articles from that news source description and time
 
-## Dev of thee app
+## Dev of the app
 python - HTML enhanced for web apps!
 flask - python micro framework.
 
---
+++ config.py
@@ -0,0 +1,34 @@
+import os
+
+class Config:
+	'''
+	General configuration parent class
+	'''
+	NEWS-HIGHLIGHT-API URL ='https://newsapi.org/v2/sources?category={}&language=en&apiKey={}'
+	NEWS_ARTICLE_API_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&language=en&apiKey={}'
+	HEADLINES_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?country=gb&category={}&apiKey={}'
+	NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    
+class ProdConfig(Config):
+	'''
+	Production configuration child class
+	
+	Args:
+		Config : The parent configuration class with General configuration settings
+	'''
+	pass
+
+
+class DevConfig(Config):
+	'''
+	Development Configuration child class
+
+	Args:
+		Config:The parent configuration class with General configuration settings
+	'''
+	DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}	
