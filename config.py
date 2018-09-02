e7dca942604fb5f6c7b0fb6d204ad4b7d21a47d8
chrikin1 <chrisianbartoo@gmail.com>
2018-09-03 00:35:50 +0300

    initial commit2

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
 	NEWS-HIGHLIGHT-API URL ='https://newsapi.org/v2/sources?category={}&language=en&apiKey={}'
 	NEWS_ARTICLE_API_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&language=en&apiKey={}'
 	HEADLINES_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?country=gb&category={}&apiKey={}'
-	NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+	NEWS_API_KEY = os.environ.get('HIGHLIGHT API_KEY')
     
 class ProdConfig(Config):
 	'''
